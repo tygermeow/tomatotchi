57f599b4ee089a67a35f337ac73e7869b51f44f6
Matsu Thornton <matsut@hawaii.edu>
2016-08-20 03:22:04 -1000

    Deleted Media added software and schematics

--- tomatotchi_firmware/tomatotchi_firmware.ino
+++ tomatotchi_firmware/tomatotchi_firmware.ino
@@ -14,9 +14,14 @@ void setup(void)
 void loop()
 {
   float temp = gettemp();
-  
+  int light = 100 - map(analogRead(A0),0,1023,10,99);
+  int moisture = 100 - map(analogRead(A1),0,1023,10,99);
   Serial.print(String(temp));
+  Serial.print(light);
+  Serial.print(moisture);
+  if(temp > 60 && temp < 80 && light > 80 && light < 95 && moisture < 95 && moisture > 70) sing(1);
+  else sing(2);
   
   
-  delay(1000);
+  delay(20000);
 }
